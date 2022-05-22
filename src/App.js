b97f299c3d50cf36b0e13bdfef2816848bfea291
ludoc0de <champi.ludo@tutanota.com>
2022-05-22 13:42:39 -0300

    comment fetch, add div element for component

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@ import Main from "./Components/Main"
 import Footer from "./Components/Footer"
 export default function App(){
     //get the data
-    
+    /*
     fetch(`https://openlibrary.org/isbn/9780140328721.json`)
      .then(res => res.json()) 
         .then(data => console.log(data))
         .catch(err => console.log(`error ${err}`));  
-    
+    */
         return(
-            <>
+            <div>
                 <Header />
                 <Main />
                 <Footer />
-            </>
+            </div>
         )
 }
