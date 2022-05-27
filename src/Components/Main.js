abb8a273139623a446d8f0b41ae09a1117af48c4
ludoc0de <champi.ludo@tutanota.com>
2022-05-27 09:12:20 -0300

    add input

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -7,10 +7,21 @@ export default function Main(){
         setButton(prevState => !prevState)
     }
 
+    function handleChange(){
+        console.log('yes')
+    }
+
     return(
         <main>
             <div>
-                <input type="text" />
+                <input 
+                    type="text" 
+                    name="isbn"
+                    placeholder="sear your book"
+                    value={isbn.search}
+                    onChange={handleChange}
+                />
+
                 <button onClick={handleClick}>
                     {button ? "wait ISBN number" : "here your book"}
                 </button>
