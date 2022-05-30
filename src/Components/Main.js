cb9978c6014ff4a66eab4662aa2a7729abf9f6f5
ludoc0de <champi.ludo@tutanota.com>
2022-05-30 02:18:19 -0300

    working on a function to get the data on clik

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,7 +3,8 @@ import React, { useEffect } from "react"
 export default function Main(){
     const[button, setButton]=React.useState(true)
     const[isbn, setIsbn]=React.useState({
-        searchBook:''
+        searchBook:'',
+        title:''
     })
 
     const [book, setBook]= React.useState([])
@@ -21,10 +22,19 @@ export default function Main(){
             const res = await fetch(`https://openlibrary.org/isbn/${isbn.searchBook}.json`)
             const data = await res.json()
             console.log(data)
-            setBook(data.title)
+            setBook(data)
         }
         getBook()
     }, [isbn])
+
+    //
+    // add another useEffect for author, fetch link above. 
+    function getBookData() {
+        const authors = book.authors[0].key
+        console.log(`https://openlibrary.org/${authors}.json`)
+        const title = book.title
+        
+    }
 
     function handleClick(){
         setButton(prevState => !prevState)
@@ -45,7 +55,7 @@ export default function Main(){
                     onChange={handleChange}
                 />
 
-                <button onClick={handleClick}>
+                <button onClick={handleClick, getBookData}>
                     {button ? "wait ISBN number" : "here your book"}
                 </button>
             </form>
