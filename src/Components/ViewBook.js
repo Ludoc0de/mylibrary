fae5586bfa880cb63d6a2b84b43d0831c7e90ea4
ludoc0de <champi.ludo@tutanota.com>
2022-06-07 00:31:20 -0300

    get all the props data from viewBook component add a section to push the props

--- src/Components/ViewBook.js
+++ src/Components/ViewBook.js
@@ -1,13 +1,16 @@
 import React from "react"
 
-export default function ViewBook(){
-    let getBooks = localStorage.getItem("books")
-    console.log(JSON.parse(getBooks))
- 
-       
+export default function ViewBook(props){
+    console.log(props)
+  
     return(
         <section>
-            <h2>GET BOOK</h2>
+            <h2>Author: {props.author}</h2>
+            <h3>Title: {props.title}</h3>
+            <p>Pages: {props.pages}</p>
+            <p>Publish date: {props.publish_date}</p>
+            <p>Format: {props.format}</p>
+            <p>Editor: {props.publisher}</p>
         </section>
     )
 }
