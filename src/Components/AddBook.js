16bebe9e72fea41243ce571b4a7b29d965b87495
ludoc0de <champi.ludo@tutanota.com>
2022-06-09 23:51:20 -0300

    delete the state with the handleClick function. Delete the button

--- src/Components/AddBook.js
+++ src/Components/AddBook.js
@@ -1,26 +1,6 @@
 import React from 'react'
 
 export default function AddBook(props){
-    const [bookId, setBookId]=React.useState(0)
-    console.log(props)
-
-    function handleClick(){
-        setBookId(prevId => prevId + 1)
-        const books = JSON.parse(localStorage.getItem("books") || "[]");
-
-        const book = {
-            id: bookId,
-            name: props.author,
-            title: props.title,
-            pages: props.pages,
-            publish_date: props.publish_date,
-            format: props.format,
-            publisher: props.publisher
-        };
-
-        books.push(book);
-        localStorage.setItem("books", JSON.stringify(books));
-    }
 
     return (
         <section>
@@ -31,7 +11,6 @@ export default function AddBook(props){
             <p>Publish date: {props.publish_date}</p>
             <p>Format: {props.format}</p>
             <p>Editor: {props.publisher}</p>
-            <button onClick={handleClick}>Add book</button>
         </section>
     )
 }
