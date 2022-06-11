031eab09c923a91bd6f306a30ad3af6938de5594
ludoc0de <champi.ludo@tutanota.com>
2022-06-11 12:08:16 -0300

    delete section add useState bookId add bookStorage() add button onClick to bookStorage

--- src/Components/AddBook.js
+++ src/Components/AddBook.js
@@ -1,16 +1,31 @@
 import React from 'react'
 
 export default function AddBook(props){
+    const [bookId, setBookId]=React.useState(0)
+
+    function bookStorage(){
+        console.log("test")
+        setBookId(prevId => prevId + 1)
+        const books = JSON.parse(localStorage.getItem("books") || "[]");
+
+        const putBook = {
+            id: bookId,
+            name: props.author,
+            title: props.title,
+            pages: props.pages,
+            publish_date: props.publish_date,
+            format: props.format,
+            publisher: props.publisher[0]
+        };
+
+        books.push(putBook);
+        localStorage.setItem("books", JSON.stringify(books));
+        window.location.reload();
+    }
 
     return (
-        <section>
-            <h2>Author: {props.author}</h2>
-            <h3>Title: {props.title}</h3>
-            {/* <p>{props.languages}</p> */}
-            <p>Pages: {props.pages}</p>
-            <p>Publish date: {props.publish_date}</p>
-            <p>Format: {props.format}</p>
-            <p>Editor: {props.publisher}</p>
-        </section>
+        <button onClick={bookStorage}>
+            Add book
+        </button>
     )
 }
