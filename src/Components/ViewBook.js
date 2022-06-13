3362e76873c08e38a9fa871ab8defe959f255d1f
ludoc0de <champi.ludo@tutanota.com>
2022-06-13 00:12:33 -0300

    add DeleteBook with id and onClick. create deleteBook(id)

--- src/Components/ViewBook.js
+++ src/Components/ViewBook.js
@@ -1,6 +1,12 @@
 import React from "react"
+import DeleteBook from "./DeleteBook"
 
 export default function ViewBook(props){
+   
+    function deleteBook(id){
+        
+    }
+
     return(
         <section>
             <h2>Author: {props.author}</h2>
@@ -9,6 +15,10 @@ export default function ViewBook(props){
             <p>Publish date: {props.publish_date}</p>
             <p>Format: {props.format}</p>
             <p>Editor: {props.publisher}</p>
+            <DeleteBook 
+                id={props.id}
+                deleteBook={deleteBook}
+            />
         </section>
     )
 }
