c81901c4514b2f6b4973f863cb11adbb6d8c1791
ludoc0de <champi.ludo@tutanota.com>
2022-06-13 15:46:53 -0300

    import css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Header from "./Components/Header"
 import Main from "./Components/Main"
 import Footer from "./Components/Footer"
+import "./style.css"
+
 export default function App(){
         return(
             <div>
