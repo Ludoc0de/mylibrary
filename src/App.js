fbbc85d304dcbc6b2d2a75609f7b7fc684375fc1
ludoc0de <champi.ludo@tutanota.com>
2022-06-27 22:15:44 -0300

    remove class container

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "./style.css"
 
 export default function App(){
         return(
-            <div className="container">
+            <div>
                 <Header />
                 <Hero />
                 <Main />
