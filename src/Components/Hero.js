0429e0763ffea9bec38abcd9e0550c916b6a56ce
ludoc0de <champi.ludo@tutanota.com>
2022-06-27 22:09:59 -0300

    add hero component

---
+++ src/Components/Hero.js
@@ -0,0 +1,11 @@
+import heroImage from "../images/reader.png"
+export default function Hero(){
+    return(
+        <section className="hero">
+            <img src={heroImage} className="hero__photo" />
+            <h2 className="hero__header">Online Experiences</h2>
+            <p className="hero__text">Join unique interactive activities led by 
+            one-of-a-kind hosts—all without leaving home.</p>
+        </section>
+    )
+}
