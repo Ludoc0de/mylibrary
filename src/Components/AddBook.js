735c46585c7ac30d61d6391d8eab05b4962f3e99
ludoc0de <champi.ludo@tutanota.com>
2022-07-16 11:51:37 -0300

    onClick event lets push data from props

--- src/Components/AddBook.js
+++ src/Components/AddBook.js
@@ -1,10 +1,8 @@
 import React, { useEffect } from 'react'
 
 export default function AddBook(props){
-    console.log(props)
     //create or get last id
     const [id, setId]= React.useState(0)
-
     useEffect(() => {
         const data = localStorage.getItem('bookId')
         setId(JSON.parse(data))
@@ -17,22 +15,20 @@ export default function AddBook(props){
     function bookStorage(){    
         console.log("test")
         setId(prevState => prevState + 1)
-        // const books = JSON.parse(localStorage.getItem("books") || "[]");
-        // const putBook = {
-        //     id: bookId,
-        //     name: props.author,
-        //     title: props.title,
-        //     pages: props.pages,
-        //     publish_date: props.publish_date,
-        //     publisher: props.publisher[0],
-        //     cover: props.cover,
-        //     count: 1
-        // };
+        const books = JSON.parse(localStorage.getItem("books") || "[]");
+        const putBook = {
+            id: id,
+            name: props.author[0],
+            title: props.title,
+            pages: props.pages,
+            publish_date: props.publish_date,
+            publisher: props.publisher[0],
+            // cover: img,
+            count: 1
+        }
+        books.push(putBook);
+        localStorage.setItem("books", JSON.stringify(books)); 
 
-        // books.push(putBook);
-        // localStorage.setItem("books", JSON.stringify(books));
-        // setBookId(prevState => prevState + 1)  
-        // window.location.reload();
     }
 
     return (
