149da927c49eb5d0795feae36e6c5c5e6102a617
ludoc0de <champi.ludo@tutanota.com>
2022-08-04 11:38:44 -0300

    update text, h2 and p

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,9 +4,13 @@ export default function Hero(){
         <section className="hero">
             <img src={heroImage} className="hero__image" />
             <div className="hero--text__container">
-                <h2 className="hero__header">Online Experiences</h2>
-                <p className="hero__text">Join unique interactive activities led by 
-                one-of-a-kind hosts—all without leaving home.</p>
+                <h2 className="hero__header">Track your library</h2>
+                <p className="hero__text">
+                    Do you remember all the books you have? <br/>
+                    On your space, you can keep your entire library! <br/>
+                    You can add new book, delete and know how many books do you have.<br/>
+                    Even though it's on the Internet, only you can see and know what books you save there.
+                </p>
             </div>
         </section>
     )
